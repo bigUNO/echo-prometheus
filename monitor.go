8a4587751bdadf60342d05a12538117f6be518a5
Leon P Johnson <ljohnson@foodlogiq.com>
2019-09-26 08:19:07 -0400

    :arrow_up: Rev bump to echo v4

--- monitor.go
+++ monitor.go
@@ -4,8 +4,8 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/prometheus/client_golang/prometheus"
 )
 
